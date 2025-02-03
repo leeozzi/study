e069902732a572a9392686ce0777448b8d93229d
leeozzi <lstory1997l@gmail.com>
2025-02-04 07:54:09 +0900

    아직 불 푸는 중

--- src/Study/algorithm_study/BOJ5427.java
+++ src/Study/algorithm_study/BOJ5427.java
@@ -10,16 +10,13 @@ import java.util.*;
 
 public class BOJ5427 {
 
-    static char[][] map;
-    static int[][] visited;
+    static Queue<Integer[]> queue; // queue에는 불 위치랑 불이 번질 위치
+    static Stack<Integer[]> stack; // stack은 상근이의 이동경로
+
+    static boolean flagBlocked = false;   // 사방이 다 막힌 경우에는 종료해주기 위한 플래그
+    static boolean flagExit = false;    // 맵 벗어나면 종료해주기 위한 플래그
+
     static int cnt;
-    static int min;
-
-    // queue에는 불 위치랑 불이 번질 위치 담아줄 거야...
-    static Queue<Integer[]> queue = new LinkedList<>();
-
-    // stack은 상근이의 이동경로
-    static Stack<Integer[]> stack = new Stack<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,9 +24,12 @@ public class BOJ5427 {
         int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
-            // tc마다 cnt랑 min 갱신
-            cnt = 0;
-            min = 0;
+            System.out.println("=======================================");
+
+
+            // queue랑 stack 새로 만들어주기
+            queue = new LinkedList<>();
+            stack = new Stack<>();
 
             // map 만들기
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -38,8 +38,12 @@ public class BOJ5427 {
             int n = h;
             int m = w;
 
-            map = new char[n][m];
-            visited = new int[n][m];
+            // tc마다 cnt랑 min 갱신
+            cnt = 0;
+            int min = n*m;
+
+            char[][] map = new char[n][m];
+            int[][] visited = new int[n][m];
 
             for (int i = 0; i < n; i++) {
                 String line = br.readLine();
@@ -48,6 +52,7 @@ public class BOJ5427 {
                 }
             }
 
+            // map 세팅
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (map[i][j] == '@') {
@@ -62,54 +67,54 @@ public class BOJ5427 {
                         visited[i][j] = 1;
                     }
                 }
+            }   // map 세팅 끝
+
+
+            // 한 가지 루트 끝날 때마다 초기 상태의 map과 visited로 돌아가서 다른 루트 카운트
+            int i = 1;
+            while (!stack.isEmpty()) {
+                // 더 갈 수 있는 데가 없거나, 탈출하면 한 가지 루트 끝난 거임
+                fire(map, visited);
+                move(map, visited);
+
+                if(flagBlocked && flagExit) {
+                    min = Math.min(min, cnt);
+
+                    flagExit = false;
+                    flagBlocked = false;
+
+                    System.out.println(i++ + "번째 루트가 끝났습니다");
+                    System.out.println(cnt + "를 0으로 초기화합니다");
+                    cnt = 0;
+                }
             }
 
-            // reset할 때 줄 default map
-            char[][] defaultMap = map;
 
-
-            while (true) {
-                // stack이 비면 완전히 모든 경로 탐색 끝난 거야.
-                if (stack.isEmpty()) {
-                    if (min > 0) {
-                        System.out.println(min);
-                        break;
-                    } else {
-                        System.out.println("IMPOSSIBLE");
-                        break;
-                    }
-                }
-
-                play(defaultMap);
+            if(min < n*m) {
+                System.out.println("현재 min은 " + min + "입니다.");
+            } else {
+                System.out.println("IMPOSSIBLE");
             }
 
         }
-    }
+    }   // main 함수
 
-    private static void play(char[][] defaultMap) {
-        bfs(defaultMap);
-        if(!stack.isEmpty()) move(defaultMap, stack.pop());
-    }
+    // 불의 이동경로 -> bfs 로 써야 한 번에 퍼뜨릴 수 있음
+    public static void fire(char[][] map, int[][] visited) {
+        for (int i = 0; i < queue.size(); i++) {
+            Integer[] fireCur = queue.poll();
 
+            int[] dx = {-1, 1, 0, 0};
+            int[] dy = {0, 0, -1, 1};
 
-    private static void bfs(char[][] map) { // 상근이 위치 받아오기
-        Integer[] fire = queue.poll();
+            for (int j = 0; j < 4; j++) {
+                int nx = fireCur[0] + dx[j];
+                int ny = fireCur[1] + dy[j];
 
-        // 사방탐색 벡터
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        // 불 사방으로 번지기 (번질 수 있는 공간이 남아있을 때만)
-        if (fire != null) {
-            for (int i = 0; i < 4; i++) {
-                int nx = fire[0] + dx[i];
-                int ny = fire[1] + dy[i];
-
-                if (nx >= 0 && nx < map.length && ny >= 0 && ny < map.length) {
+                if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
                     if (visited[nx][ny] != 1) {
-                        queue.poll();
-                        Integer[] spread = {nx, ny};
-                        queue.add(spread);
+                        Integer[] next = {nx, ny};
+                        queue.add(next);
                         visited[nx][ny] = 1;
                     }
                 }
@@ -117,44 +122,31 @@ public class BOJ5427 {
         }
     }
 
-    private static void move(char[][] map, Integer[] cur) {
-        // 얘는 dfs 써야할 것 같음
-        // 사방탐색 벡터
+    // 상근이의 이동경로 -> dfs 로 넣어줘야 끝까지 갈 수 있음
+    public static void move(char[][] map, int[][] visited) {
+        Integer[] cur = stack.pop();
+        System.out.println(cur[0] + " " + cur[1]);
+        cnt++;
+
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
 
-        // 바로 다음 턴에 갈 수 있는 위치들을 stack에 넣어주기
         for (int i = 0; i < 4; i++) {
-            boolean flag = false;
             int nx = cur[0] + dx[i];
             int ny = cur[1] + dy[i];
 
-            if (nx >= 0 && nx < map.length && ny >= 0 && ny < map.length) {
+            if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
                 if (visited[nx][ny] != 1) {
                     Integer[] next = {nx, ny};
-                    if (visited[nx][ny] != 1) {
-                        stack.add(next);
-                        flag = true;
-                        // 사방 탐색하면서 하나라도 추가 가능한 거 발견하면 true로 돌려줌
-                    }
+                    stack.add(next);
+                    System.out.println("방금 stack에 {" + nx + " " + ny + "} 추가함");
+                    visited[nx][ny] = 1;
+                    flagBlocked = true;    // 다음에 갈 수 있는 곳이 하나라도 있으면 true
                 }
-
-                // 아직 맵 안 벗어나는데 사방 가로막히면 죽은 거
-                // -> 이 가로막힌 걸 알 수 있게 flag 심어주자
-                // 죽고 나면...
-                // 그 다음 stack에 있는 애 즉, 아까 가능했는데 선택하지 않았던 애한테 가겠지?
-                // 그럼 다시 리셋해줘야 해.
-                if (flag == false) {
-                    play(map);
-                }
-
-
-            } else {    // nx나 ny가 map 범위 벗어나면 일단 min 저장해주고
-                min = Math.min(min, cnt);
-                // 이 stack에 남은 건 계속 가져가면서
-                // map이랑 visited는 리셋해줘야 함
-                play(map);
+            } else {
+                flagExit = true;    // nx, ny가 위 범위를 벗어났다는 말은 내가 끝자락에 다왔다는 얘기
             }
         }
     }
+
 }
