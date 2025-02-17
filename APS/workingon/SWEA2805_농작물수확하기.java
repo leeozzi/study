dbf6d9b595f6756a93cbaef7d7028546ff5ef9f8
leeozzi <lstory1997l@gmail.com>
2025-02-17 17:25:48 +0900

    0217

--- APS/workingon/SWEA2805_농작물수확하기.java
+++ APS/workingon/SWEA2805_농작물수확하기.java
@@ -4,52 +4,50 @@ import java.io.*;
 
 public class SWEA2805_농작물수확하기 {
 
-    static int[][] map;
-    static int[][] visited;
-    static int sum;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        for(int t = 1; t <= T ; t++) {
+        for (int t = 1; t <= T; t++) {
             // map 만들어줄게
             int N = Integer.parseInt(br.readLine());
-            map = new int[N][N];
-            for(int i = 0 ; i < N ; i++) {
+            int[][] map = new int[N][N];
+            for (int i = 0; i < N; i++) {
                 String str = br.readLine();
                 String[] strs = str.split("");
-                for(int j = 0 ; j < N ; j++){
+                for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(strs[j]);
                 }
             }
 
-            // 중앙 기준으로 마름모로 퍼지면서 더해줄게
-            // 중앙 위치 받아서 상하좌우 퍼지는 걸 함수로 돌려야겠다
-            visited = new int[N][N];
-            sum = 0;
-            sum = map[N/2][N/2];
-            spread(N/2, N/2);
-            System.out.println(sum);
-        }
-    }
+            // 시작점을 처음에 젤 중앙에 두고
+            // 길이만큼 반복하면서 더해줘
+            // 시작점 한 칸 땡기면서 길이 2개씩 늘려줘
+            // i = N/2 번째 줄 도달하면, 이제 담부터 길이 -2씩 해줄 거야
+            int sum = 0;
+            int startpoint = N / 2;
+            int len = 1;
+            int plus = 2;
+            int move = -1;
+            for (int i = 0; i < N; i++) {
+                for (int j = startpoint; j < startpoint + len - 1; j++) {
+                    sum += map[i][startpoint];
+                    System.out.print(map[i][startpoint] + " ");
+                }
 
-    public static void spread(int i, int j) {
-        visited[i][j] = 1;
+                startpoint += move;
+                len += plus;
 
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-
-        for(int d = 0 ; d < 4 ; d++) {
-            int nx = i + dx[d];
-            int ny = j + dy[d];
-            if(nx>=0  && ny >=0 && nx < map.length && ny < map.length){
-                if(visited[nx][ny] != 1) {
-                    sum += map[nx][ny];
-                    spread(nx, ny);
+                if(startpoint < 0) {
+                    startpoint = 1;
+                    plus = -2;
+                    move = 1;
                 }
             }
+
+            System.out.println();
+            System.out.println(sum);
         }
     }
 }
