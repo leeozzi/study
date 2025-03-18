fc9c37c45f811272900b1511433dbfb5d03a20f4
leeozzi <lstory1997l@gmail.com>
2025-03-19 06:58:39 +0900

    0318

--- APS/BOJ/BOJ12907_동물원.java
+++ APS/BOJ/BOJ12907_동물원.java
@@ -1,11 +1,10 @@
-package workingon;
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class BOJ12907_동물원_X {
+public class BOJ12907_동물원 {
 
     static int N;
     static int[] arr, res;
@@ -38,13 +37,12 @@ public class BOJ12907_동물원_X {
             }
 
             // cnt 2가 끝나는 위치, cnt 1이 끝나는 위치 저장 -> res 개수로 만들어줄 거라 +1
-            if (i != 0) {
-                if(cnt[i] == 2) num1 = i+1;
-                else if (cnt[i] == 1) num2 = i+1;
-            }
+            if (cnt[i] == 2) {
+                num1 = i + 1;
+            } else if (cnt[i] == 1) num2 = i + 1;
         }
 
-        if(num1 != 0) {
+        if (num1 != 0) {
             res = new int[num1];
         } else {
             res = new int[num2];
@@ -53,7 +51,7 @@ public class BOJ12907_동물원_X {
         // 조합 만들기
         // 더 작은 그룹에 채워넣기 * 2 - 토끼 고양이
         zoo(0);
-        if(num1 != N/2)
+        if (num1*2 != N)
             System.out.println(ans * 2);
         else
             System.out.println(ans);
@@ -63,7 +61,6 @@ public class BOJ12907_동물원_X {
 
     public static void zoo(int index) {
         if (index == num1) {
-            System.out.println(Arrays.toString(res));
             ans++;
             return;
         }
--- APS/workingon/BOJ9663_NQueen_메모리초과.java
+++ APS/workingon/BOJ9663_NQueen_메모리초과.java
@@ -1,4 +1,4 @@
-package BOJ;
+package workingon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
