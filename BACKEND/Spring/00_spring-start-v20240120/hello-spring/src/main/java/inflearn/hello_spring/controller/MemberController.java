92d24ccd607ee908b89ea64359c22fefd5ed76b3
leeozzi <lstory1997l@gmail.com>
2025-04-03 09:01:27 +0900

    0403

--- BACKEND/Spring/00_spring-start-v20240120/hello-spring/src/main/java/inflearn/hello_spring/controller/MemberController.java
+++ BACKEND/Spring/00_spring-start-v20240120/hello-spring/src/main/java/inflearn/hello_spring/controller/MemberController.java
@@ -1,9 +1,11 @@
 package inflearn.hello_spring.controller;
 
+import inflearn.hello_spring.domain.Member;
 import inflearn.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -22,5 +24,12 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createFrom() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        return "";
     }
 }
