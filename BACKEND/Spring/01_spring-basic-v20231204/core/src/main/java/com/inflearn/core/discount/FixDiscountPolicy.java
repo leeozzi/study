c9b3a4ae72bafa82ec69307b4e06fb7a4bf2b907
leeozzi <lstory1997l@gmail.com>
2025-04-22 07:19:28 +0900

    0421

--- BACKEND/Spring/01_spring-basic-v20231204/core/src/main/java/com/inflearn/core/discount/FixDiscountPolicy.java
+++ BACKEND/Spring/01_spring-basic-v20231204/core/src/main/java/com/inflearn/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.inflearn.core.discount;
 
 import com.inflearn.core.member.Grade;
 import com.inflearn.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000;   // 1000원 할인
 
--- BACKEND/Spring/01_spring-basic-v20231204/core/src/main/java/com/inflearn/core/order/OrderServiceImpl.java
+++ BACKEND/Spring/01_spring-basic-v20231204/core/src/main/java/com/inflearn/core/order/OrderServiceImpl.java
@@ -6,12 +6,16 @@ import com.inflearn.core.discount.RateDiscountPolicy;
 import com.inflearn.core.member.Member;
 import com.inflearn.core.member.MemberRepository;
 import com.inflearn.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
